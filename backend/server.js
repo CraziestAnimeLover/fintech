726cf87318b27fbbad8085a3f63e9b6cb38428ed
CraziestAnimeLover <riyarsunny706@gmail.com>
2026-03-18 12:02:02 +0530

    upadte the frontend origin

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use(
 /* ---------------- CORS ---------------- */
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://fintech-craziestanimelovers-projects.vercel.app"],
+    origin: ["http://localhost:3000","https://fintech-craziestanimelovers-projects.vercel.app","https://fintech-kappa-two.vercel.app"],
     credentials: true,
   })
 );
