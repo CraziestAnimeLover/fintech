57dd9f27b257d78c7038e11f99c3c48e5e1a590d
CraziestAnimeLover <riyarsunny706@gmail.com>
2026-03-24 10:32:02 +0530

    add vite build file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
   define: {
-    'process.env': process.env, // optional for accessing env vars
+    'process.env': process.env,
   },
-
-
+  build: {
+    outDir: "dist"
+  },
   server: {
     port: 3000,
-    host: true, // exposes dev server to network (useful for devices/Docker)
+    host: true,
     proxy: mode === 'development' ? {
       '/api': {
-        target: 'https://fintech-6bvt.onrender.com', // backend port
+        target: 'https://fintech-6bvt.onrender.com',
         changeOrigin: true,
-        secure: false, // avoid SSL issues in dev
-        rewrite: path => path.replace(/^\/api/, '/api'), // keeps /api path intact
+        secure: false,
       }
     } : undefined
   }
